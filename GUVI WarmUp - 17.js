52c23bf9031974408aa262920fb2ffe3b9eed080
Azhagu Karthikeyan B <128957710+azhagu-akk@users.noreply.github.com>
2024-08-01 17:33:31 +0530

    Update GUVI WarmUp - 17.js

--- GUVI WarmUp - 17.js
+++ GUVI WarmUp - 17.js
@@ -8,7 +8,7 @@ function getDistance(x1, y1, x2, y2) {
 	
 	let d = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)))
 	
-	return Math.parseInt(d)
+	return parseInt(d)
 	
 	///Your code Ends here
 }
