5045823a3c57a0507e3f7db266c1ed9e871b2396
Azhagu <azhagukarthikeyanb@gmail.com>
2024-08-01 13:10:03 +0530

    Added: WarmUp Assessment

---
+++ GUVI WarmUp - 11.js
@@ -0,0 +1,17 @@
+/* Create a function that returns the number of frames shown in a given number of minutes for a certain FPS. */
+
+function frames(num1, num2) {
+	///Your code Starts here 
+	let min = num1 * 60
+	
+	let fps = min * num2
+	
+	return fps
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+frames(1, 1);
+frames(10, 1);
+frames(10, 25); 
+*/
