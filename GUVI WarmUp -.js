1bf39b0fb0050c732c898c4791c1e044bcc61b21
Azhagu Karthikeyan B <128957710+azhagu-akk@users.noreply.github.com>
2024-08-02 21:08:45 +0530

    Add files via upload

---
+++ GUVI WarmUp -.js
@@ -0,0 +1,17 @@
+/* Given an object and a key, "removeProperty" removes the given key from the given object. */
+
+var obj = {
+	"mykey": "value"
+}
+
+function removeProperty(obj, key) {
+	///Your code Starts here 
+	delete obj[key]
+	return obj
+	///Your code Ends here
+}
+removeProperty(obj, "mykey");
+/*
+Lines For TestCase
+obj.mykey === undefined;
+*/
